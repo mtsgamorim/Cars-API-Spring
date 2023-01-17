498bab6846ac2ce6fcfc322ab9fa472784de6b63
Matheus Amorim <mtsgamorim@gmail.com>
2023-01-17 15:24:01 -0300

    post in '/'

---
+++ src/main/java/com/carsapi/api/controller/CarController.java
@@ -0,0 +1,22 @@
+package com.carsapi.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.carsapi.api.dto.CarDTO;
+
+@RequestMapping("/")
+@RestController
+public class CarController {
+    
+    @PostMapping
+    public void createCar(@RequestBody CarDTO req) {
+        System.out.println(req.modelo());
+        System.out.println(req.fabricante());
+        System.out.println(req.dataFabricacao());
+        System.out.println(req.valor());
+        System.out.println(req.anoModelo());
+    }
+}
